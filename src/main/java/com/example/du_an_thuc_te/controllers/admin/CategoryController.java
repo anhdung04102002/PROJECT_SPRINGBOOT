25eb54b4ac02fd9985724100d80f72dde445d805
aa <anhdung04102002@gmail.com>
2024-02-24 18:11:51 +0700

    hoàn thành CRUD cho quản lí danh mục

--- src/main/java/com/example/du_an_thuc_te/controllers/admin/CategoryController.java
+++ src/main/java/com/example/du_an_thuc_te/controllers/admin/CategoryController.java
@@ -4,12 +4,14 @@ import com.example.du_an_thuc_te.Service.CategoryService;
 import com.example.du_an_thuc_te.models.Categories;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @Controller
+@Service
 public class CategoryController {
     @Autowired
     private CategoryService categoryService; // thông qua lớp giao diện
@@ -19,9 +21,35 @@ public class CategoryController {
         model.addAttribute("list",list);
         return "admin/category/index";
     }
+    // phải request đến trang add trước rồi khi vào đó mới thực hiện hành động post được
     @RequestMapping("/admin/add-category")
-    public String add() {
+    public String add(Model model) {
+        Categories categories = new Categories();
+        // cài sẵn trường cho status là true (hiện) để khi vào có sẵn tích ở hiện
+        categories.setCategoryStatus(true);
+        model.addAttribute("category",categories);
         return "admin/category/add";
+    }
+    @PostMapping("/admin/add-category/saveCategories")
+    public String saveCategory(@ModelAttribute("category") Categories categories) {
+            categoryService.saveCategories(categories);
+            return "redirect:/admin/category";
+    }
+    @GetMapping("/admin/category/delete/{id}")
+    public String delete(@PathVariable(value = "id") int id) {
+            this.categoryService.deleteCategories(id);
+            return "redirect:/admin/category";
+    }
+    @GetMapping("/admin/category/update/{id}")
+    public String showFormUpdate(@PathVariable(value = "id") int id, Model model) {
+            Categories categories = categoryService.getCategoriesById(id);
+            model.addAttribute("category",categories);
+            return "admin/category/update";
+    }
+    @PostMapping("/admin/categogy/update")
+    public  String saveUpdate(@ModelAttribute("categoryUpdate") Categories categories) {
+        categoryService.saveCategories(categories);
+        return "redirect:/admin/category";
     }
 
 }
